537801f907f7f52dcc58776053202e471dbc0638
hokole <94093120+hokole@users.noreply.github.com>
2023-09-06 18:58:59 -0500

    Successfully implemented firebase auth

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from shopping import views
 
 urlpatterns = [
-    path("shopping/", include("shopping.urls")),
     path('admin/', admin.site.urls),
+    # Here we are assigning the path of our url
+    path('', views.signIn),
+    path('postsignIn/', views.postsignIn),
+    path('signUp/', views.signUp, name="signup"),
+    path('logout/', views.logout, name="log"),
+    path('postsignUp/', views.postsignUp),
 ]
--- shop/shopping/urls.py
+++ shop/shopping/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('admin/', admin.site.urls),
+    # Here we are assigning the path of our url
+    path('signIn/', views.signIn),
+    path('postsignIn/', views.postsignIn),
+    path('signUp/', views.signUp, name="signup"),
+    path('logout/', views.logout, name="log"),
+    path('postsignUp/', views.postsignUp),
 ]
